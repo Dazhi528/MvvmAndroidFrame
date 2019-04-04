6304fbfb2c9af64b95c714190c880f056eda3238
wzz528 <wzz528@icloud.com>
2019-04-04 17:02:25 +0800

    增加路由库

--- library/src/main/java/com/dazhi/libroot/root/RootApp.java
+++ library/src/main/java/com/dazhi/libroot/root/RootApp.java
@@ -3,6 +3,8 @@ package com.dazhi.libroot.root;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.support.multidex.MultiDexApplication;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.dazhi.libroot.util.UtLog;
 import com.dazhi.libroot.util.UtRoot;
 import com.uuzuche.lib_zxing.activity.ZXingLibrary;
 
@@ -22,6 +24,12 @@ public abstract class RootApp extends MultiDexApplication {
         //
         UtRoot.initApp(this);
         ZXingLibrary.initDisplayOpinion(this);
+        // ARouter
+        if (UtLog.booDebug()) { // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog();  // 打印日志
+            ARouter.openDebug(); // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(this); // 尽可能早，推荐在Application中初始化
     }
 
     @Override
