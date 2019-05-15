6bde144dc951c569283c1183b3da035cb8a13ebf
wzz528 <wzz528@icloud.com>
2019-05-15 16:49:25 +0800

    优化编码工具

--- library/src/main/java/com/dazhi/libroot/util/UtCode.java
+++ library/src/main/java/com/dazhi/libroot/util/UtCode.java
@@ -1,7 +1,7 @@
 package com.dazhi.libroot.util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
+import android.util.Base64;
+import java.security.MessageDigest;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -83,120 +83,171 @@ public class UtCode {
     }
 
 
+    /**=======================================
+     * 作者：WangZezhi  (2019-05-15  16:08)
+     * 功能：Base64 编码/解码
+     * 描述：
+     *=======================================*/
+    // 编码
+    public static String base64Encode(final String input) {
+        return base64Encode(input.getBytes());
+    }
+    public static String base64Encode(final byte[] input) {
+        if (input == null || input.length == 0) return "";
+        return Base64.encodeToString(input, Base64.NO_WRAP);
+    }
 
-    /**===============================================
-     * 作者：WangZezhi  (17-5-26  上午11:36)
-     * 功能：Base64编码/解码部分
-     * 详情：合法的（legal）
-     ================================================*/
-    private static final char[] CARR_LEGAL_CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
-    /**
-     * data[]进行编码
-     * @param data
-     * @return
-     */
-    public static String base64Encode(byte[] data) {
-        if(data==null){
-            return "";
-        }
-        int start = 0;
-        int len = data.length;
-        StringBuffer buf = new StringBuffer(data.length * 3 / 2);
-        int end = len - 3;
-        int i = start;
-        int n = 0;
-        while (i <= end) {
-            int d = ((((int) data[i]) & 0x0ff) << 16)
-                    | ((((int) data[i + 1]) & 0x0ff) << 8)
-                    | (((int) data[i + 2]) & 0x0ff);
-            buf.append(CARR_LEGAL_CHAR[(d >> 18) & 63]);
-            buf.append(CARR_LEGAL_CHAR[(d >> 12) & 63]);
-            buf.append(CARR_LEGAL_CHAR[(d >> 6) & 63]);
-            buf.append(CARR_LEGAL_CHAR[d & 63]);
-            i += 3;
-            if (n++ >= 14) {
-                n = 0;
-                buf.append(" ");
-            }
-        }
-        if (i == start + len - 2) {
-            int d = ((((int) data[i]) & 0x0ff) << 16)
-                    | ((((int) data[i + 1]) & 255) << 8);
-            buf.append(CARR_LEGAL_CHAR[(d >> 18) & 63]);
-            buf.append(CARR_LEGAL_CHAR[(d >> 12) & 63]);
-            buf.append(CARR_LEGAL_CHAR[(d >> 6) & 63]);
-            buf.append("=");
-        } else if (i == start + len - 1) {
-            int d = (((int) data[i]) & 0x0ff) << 16;
-            buf.append(CARR_LEGAL_CHAR[(d >> 18) & 63]);
-            buf.append(CARR_LEGAL_CHAR[(d >> 12) & 63]);
-            buf.append("==");
-        }
-        return buf.toString();
+    // 解码
+    public static byte[] base64Decode(final String input) {
+        if (input == null || input.length() == 0) return new byte[0];
+        return Base64.decode(input, Base64.NO_WRAP);
+    }
+    public static byte[] base64Decode(final byte[] input) {
+        if (input == null || input.length == 0) return new byte[0];
+        return Base64.decode(input, Base64.NO_WRAP);
     }
 
 
     /**=======================================
-     * 作者：WangZezhi  (2018/6/8  17:22)
-     * 功能：base64Decode
+     * 作者：WangZezhi  (2019-05-15  16:23)
+     * 功能：MD5
      * 描述：
      *=======================================*/
-    public static byte[] base64Decode(String s) throws Exception {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        base64Decode(s, bos);
-        byte[] decodedBytes = bos.toByteArray();
-        bos.close();
-        bos = null;
-        return decodedBytes;
+    public static String md5(String strText) {
+        return md5(strText, "I1j0l9n4DF3Aac|spP03DGqd4msbb");
     }
-    //
-    private static void base64Decode(String s, OutputStream os) throws Exception {
-        int i = 0;
-        int len = s.length();
-        while (true) {
-            while (i < len && s.charAt(i) <= ' ') {
-                i++;
+    public static String md5(String strText, String strSecret) {
+        try {
+            strText = strText + strSecret;
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(strText.getBytes("UTF8"));
+            byte s[] = m.digest();
+            String result = "";
+            for (int i = 0; i < s.length; i++) {
+                result += Integer.toHexString((0x000000FF & s[i]) | 0xFFFFFF00).substring(6);
             }
-            if (i == len) {
-                break;
-            }
-            int tri = (base64Decode(s.charAt(i)) << 18)
-                    + (base64Decode(s.charAt(i + 1)) << 12)
-                    + (base64Decode(s.charAt(i + 2)) << 6)
-                    + (base64Decode(s.charAt(i + 3)));
-            os.write((tri >> 16) & 255);
-            if (s.charAt(i + 2) == '=') {
-                break;
-            }
-            os.write((tri >> 8) & 255);
-            if (s.charAt(i + 3) == '=') {
-                break;
-            }
-            os.write(tri & 255);
-            i += 4;
-        }
-    }
-    //
-    private static int base64Decode(char c) {
-        if (c >= 'A' && c <= 'Z') {
-            return ((int) c) - 65;
-        } else if (c >= 'a' && c <= 'z') {
-            return ((int) c) - 97 + 26;
-        } else if (c >= '0' && c <= '9') {
-            return ((int) c) - 48 + 26 + 26;
-        } else {
-            switch (c) {
-                case '+':
-                    return 62;
-                case '/':
-                    return 63;
-                case '=':
-                    return 0;
-                default:
-                    throw new RuntimeException("unexpected code: " + c);
-            }
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
         }
     }
 
 
 }
+
+
+
+/**===============================================
+ * 作者：WangZezhi  (17-5-26  上午11:36)
+ * 功能：Base64编码/解码部分
+ * 详情：合法的（legal）
+ ================================================*/
+//    private static final char[] CARR_LEGAL_CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
+//    /**
+//     * data[]进行编码
+//     * @param data
+//     * @return
+//     */
+//    public static String base64Encode(byte[] data) {
+//        if(data==null){
+//            return "";
+//        }
+//        int start = 0;
+//        int len = data.length;
+//        StringBuffer buf = new StringBuffer(data.length * 3 / 2);
+//        int end = len - 3;
+//        int i = start;
+//        int n = 0;
+//        while (i <= end) {
+//            int d = ((((int) data[i]) & 0x0ff) << 16)
+//                    | ((((int) data[i + 1]) & 0x0ff) << 8)
+//                    | (((int) data[i + 2]) & 0x0ff);
+//            buf.append(CARR_LEGAL_CHAR[(d >> 18) & 63]);
+//            buf.append(CARR_LEGAL_CHAR[(d >> 12) & 63]);
+//            buf.append(CARR_LEGAL_CHAR[(d >> 6) & 63]);
+//            buf.append(CARR_LEGAL_CHAR[d & 63]);
+//            i += 3;
+//            if (n++ >= 14) {
+//                n = 0;
+//                buf.append(" ");
+//            }
+//        }
+//        if (i == start + len - 2) {
+//            int d = ((((int) data[i]) & 0x0ff) << 16)
+//                    | ((((int) data[i + 1]) & 255) << 8);
+//            buf.append(CARR_LEGAL_CHAR[(d >> 18) & 63]);
+//            buf.append(CARR_LEGAL_CHAR[(d >> 12) & 63]);
+//            buf.append(CARR_LEGAL_CHAR[(d >> 6) & 63]);
+//            buf.append("=");
+//        } else if (i == start + len - 1) {
+//            int d = (((int) data[i]) & 0x0ff) << 16;
+//            buf.append(CARR_LEGAL_CHAR[(d >> 18) & 63]);
+//            buf.append(CARR_LEGAL_CHAR[(d >> 12) & 63]);
+//            buf.append("==");
+//        }
+//        return buf.toString();
+//    }
+
+
+/**=======================================
+ * 作者：WangZezhi  (2018/6/8  17:22)
+ * 功能：base64Decode
+ * 描述：
+ *=======================================*/
+//    public static byte[] base64Decode(String s) throws Exception {
+//        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//        base64Decode(s, bos);
+//        byte[] decodedBytes = bos.toByteArray();
+//        bos.close();
+//        bos = null;
+//        return decodedBytes;
+//    }
+//    //
+//    private static void base64Decode(String s, OutputStream os) throws Exception {
+//        int i = 0;
+//        int len = s.length();
+//        while (true) {
+//            while (i < len && s.charAt(i) <= ' ') {
+//                i++;
+//            }
+//            if (i == len) {
+//                break;
+//            }
+//            int tri = (base64Decode(s.charAt(i)) << 18)
+//                    + (base64Decode(s.charAt(i + 1)) << 12)
+//                    + (base64Decode(s.charAt(i + 2)) << 6)
+//                    + (base64Decode(s.charAt(i + 3)));
+//            os.write((tri >> 16) & 255);
+//            if (s.charAt(i + 2) == '=') {
+//                break;
+//            }
+//            os.write((tri >> 8) & 255);
+//            if (s.charAt(i + 3) == '=') {
+//                break;
+//            }
+//            os.write(tri & 255);
+//            i += 4;
+//        }
+//    }
+//    //
+//    private static int base64Decode(char c) {
+//        if (c >= 'A' && c <= 'Z') {
+//            return ((int) c) - 65;
+//        } else if (c >= 'a' && c <= 'z') {
+//            return ((int) c) - 97 + 26;
+//        } else if (c >= '0' && c <= '9') {
+//            return ((int) c) - 48 + 26 + 26;
+//        } else {
+//            switch (c) {
+//                case '+':
+//                    return 62;
+//                case '/':
+//                    return 63;
+//                case '=':
+//                    return 0;
+//                default:
+//                    throw new RuntimeException("unexpected code: " + c);
+//            }
+//        }
+//    }
