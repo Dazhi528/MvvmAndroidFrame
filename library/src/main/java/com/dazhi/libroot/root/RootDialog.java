5d8b5f3ca1073b4fbf93d3f56ad450ba2319abd0
wzz528 <wzz528@icloud.com>
2019-05-18 11:17:30 +0800

    基础对话框增加辅助方法

--- library/src/main/java/com/dazhi/libroot/root/RootDialog.java
+++ library/src/main/java/com/dazhi/libroot/root/RootDialog.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.v7.app.AppCompatDialog;
 import android.view.Window;
 import com.dazhi.libroot.R;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 /**
  * 功能：
@@ -14,6 +16,8 @@ import com.dazhi.libroot.R;
  * 修改日期：2018/4/11 15:32
  */
 public abstract class RootDialog extends AppCompatDialog {
+    //每一个p都去创建本地的CompositeDisposable，从而释放资源时，不相互影响
+    private CompositeDisposable compositeDisposable;
 
     protected RootDialog(Context context) {
         super(context, R.style.LibRootDialogBaseStyle);
@@ -22,9 +26,36 @@ public abstract class RootDialog extends AppCompatDialog {
         if(window!=null){
             window.setWindowAnimations(R.style.LibRootDialogAnimScale);
         }
+        //
+        initViewAndDataAndEvent();
     }
 
     protected abstract int initLayout();
+    protected void initViewAndDataAndEvent() {
+    }
+
+
+    /**=======================================
+     * 作者：WangZezhi  (2018/7/2  11:45)
+     * 功能：本方法用于统一管理Rx事件
+     * 描述：
+     *=======================================*/
+    protected void rxAdd(Disposable subscription) {
+        if (compositeDisposable == null) {
+            compositeDisposable = new CompositeDisposable();
+        }
+        compositeDisposable.add(subscription);
+    }
+
+    @Override
+    public void dismiss() {
+        if (compositeDisposable != null) {
+            compositeDisposable.clear();
+            compositeDisposable=null;
+        }
+        //
+        super.dismiss();
+    }
 
 
 }
