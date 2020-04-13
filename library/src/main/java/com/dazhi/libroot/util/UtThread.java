c6d937ea14628bd7028d47d33e0e56a7fb5020a2
dazhi <1041665128@qq.com>
2020-04-13 09:32:36 +0800

    线程池工具修改优化,不兼容之前

--- library/src/main/java/com/dazhi/libroot/util/UtThread.java
+++ library/src/main/java/com/dazhi/libroot/util/UtThread.java
@@ -2,6 +2,9 @@ package com.dazhi.libroot.util;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 功能：线程池工具
@@ -12,15 +15,44 @@ import java.util.concurrent.Executors;
  * 修改日期：2018/9/17 14:11
  */
 public class UtThread {
-    private static final ExecutorService SINGLE_THREAD_EXECUTOR = Executors.newSingleThreadExecutor();
+    // 如果不调用初始化,默认是newSingleThreadExecutor配置
+    private static int corePoolSize=1;
+    private static int maximumPoolSize=1;
+    private static long keepAliveTime=0;
+    private final ExecutorService THREAD_POOL_EXECUTOR;
 
-    public static void runSingleThread(Runnable runnable){
-        SINGLE_THREAD_EXECUTOR.execute(runnable);
+    private UtThread() {
+        THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
+                keepAliveTime, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+    }
+    private static final class ClassHolder {
+        static final UtThread INSTANCE = new UtThread();
+    }
+    public static void init(int corePoolSize, int maximumPoolSize, long keepAliveTime) {
+        UtThread.corePoolSize = corePoolSize;
+        UtThread.maximumPoolSize = maximumPoolSize;
+        UtThread.keepAliveTime = keepAliveTime;
+    }
+    public static UtThread self() {
+        return ClassHolder.INSTANCE;
     }
 
-    public static void shutdownNow(){
-        SINGLE_THREAD_EXECUTOR.shutdownNow();
+
+    public void execute(Runnable runnable){
+        THREAD_POOL_EXECUTOR.execute(runnable);
     }
 
+    public boolean isShutdown() {
+        return THREAD_POOL_EXECUTOR.isShutdown();
+    }
+
+    public void shutdown(){
+        THREAD_POOL_EXECUTOR.shutdown();
+    }
+
+    public void shutdownNow(){
+        THREAD_POOL_EXECUTOR.shutdownNow();
+    }
 
 }
